73c51523a65cfd8ae891df473e37680ae78bebc6
enrico-fornasa <enrico.fornasa@studenti.unitn.it>
2022-05-15 22:49:22 +0200

    Compilato layoutPersonale.html

--- app/app.js
+++ app/app.js
@@ -1,28 +1,21 @@
 const express = require('express');
 const app = express();
 
-
-
 /**
  * Configure Express.js parsing middleware
  */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 /**
  * Serve front-end static files
  */
 app.use('/', express.static('static'));
-
-
 
 /* Default 404 handler */
 app.use((req, res) => {
     res.status(404);
     res.json({ error: 'Not found' });
 });
-
-
 
 module.exports = app;
