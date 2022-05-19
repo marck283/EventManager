ada0bba632048d03d0c31365c90bce5c56d36be4
enrico-fornasa <enrico.fornasa@studenti.unitn.it>
2022-05-19 22:19:52 +0200

    Aggiunte gestioni degli errori/token

--- app/infoEventiPersonal.js
+++ app/infoEventiPersonal.js
@@ -4,18 +4,25 @@ const EventPers = require('./collezioni/eventpersonal');
 const Users = require('./collezioni/utenti');
 
 router.get('/:id', async(req, res) => {
-    let eventoPersonale = await EventPers.findById(req.params.id);
-    let organizzatore = await Users.findById(eventoPersonale.organizzatoreID);
 
-    res.status(200).json({
-        nomeAtt: eventoPersonale.nomeAtt,
-        categoria: eventoPersonale.categoria,
-        data: eventoPersonale.data,
-        ora: eventoPersonale.ora,
-        durata: eventoPersonale.durata,
-        luogoEv: eventoPersonale.luogoEv,
-        organizzatore: organizzatore.nome,
-    });
+    try{
+        let eventoPersonale = await EventPers.findById(req.params.id);
+        let organizzatore = await Users.findById(eventoPersonale.organizzatoreID);
+
+        res.status(200).json({
+            nomeAtt: eventoPersonale.nomeAtt,
+            categoria: eventoPersonale.categoria,
+            data: eventoPersonale.data,
+            ora: eventoPersonale.ora,
+            durata: eventoPersonale.durata,
+            luogoEv: eventoPersonale.luogoEv,
+            organizzatore: organizzatore.nome,
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error: "Errore nel Server"}).send();
+    }
+
 });
 
 module.exports = router;
--- static/scripts/scriptInfoEventPub.js
+++ static/scripts/scriptInfoEventPub.js
@@ -1,39 +1,62 @@
 let url = window.location.href;
-url=url.split('?');
-url=url[1].split('=');
-id = url[1];
+var id = '';
+var token = '';
+
+try{
+    url=url.split('?');
+    url=url[1].split('&');
+    idurl=url[0].split('=');
+    tokenurl=url[1].split('=');
+    id=idurl[1];
+    token=tokenurl[1];
+}catch(error){
+    console.log(error);
+}
+
 fetch('../api/v1/EventiPubblici/'+id)
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(resp) {
+    .then(resp => {
+        switch(resp.status){
+            case 200: {
+                resp.json().then(resp => {
+                    let nomeAtt = document.getElementsByTagName("h1");
+                    nomeAtt[0].textContent = resp.nomeAtt;
 
-        let nomeAtt = document.getElementsByTagName("h1");
-        nomeAtt[0].textContent = resp.nomeAtt;
+                    let data = document.getElementById("data");
+                    data.textContent = resp.data;
+                    let ora = document.getElementById("ora");
+                    ora.textContent = resp.ora;
+                    let durata = document.getElementById("durata");
+                    durata.textContent = resp.durata;
 
-        let data = document.getElementById("data");
-        data.textContent = resp.data;
-        let ora = document.getElementById("ora");
-        ora.textContent = resp.ora;
-        let durata = document.getElementById("durata");
-        durata.textContent = resp.durata;
+                    let categoria = document.getElementById("categoria");
+                    categoria.textContent = resp.categoria;
 
-        let categoria = document.getElementById("categoria");
-        categoria.textContent = resp.categoria;
+                    let indirizzo = document.getElementById("indirizzo");
+                    indirizzo.textContent = resp.luogoEv.indirizzo;
+                    let citta = document.getElementById("citta");
+                    citta.textContent = resp.luogoEv.citta;
 
-        let indirizzo = document.getElementById("indirizzo");
-        indirizzo.textContent = resp.luogoEv.indirizzo;
-        let citta = document.getElementById("citta");
-        citta.textContent = resp.luogoEv.citta;
+                    let organizzatore = document.getElementById("organizzatore");
+                    organizzatore.textContent = resp.organizzatore;
 
-        let organizzatore = document.getElementById("organizzatore");
-        organizzatore.textContent = resp.organizzatore;
+                    let maxPers = document.getElementById("maxPers");
+                    maxPers.textContent = resp.maxPers;
+                    let numPers = document.getElementById("numPers");
+                    numPers.textContent = resp.partecipanti.length;
 
-        let maxPers = document.getElementById("maxPers");
-        maxPers.textContent = resp.maxPers;
-        let numPers = document.getElementById("numPers");
-        numPers.textContent = resp.partecipanti.length;
-
-        let partecipanti = document.getElementById("partecipanti");
-        partecipanti.textContent = resp.partecipanti;
-
+                    let partecipanti = document.getElementById("partecipanti");
+                    partecipanti.textContent = resp.partecipanti;
+                });
+                break;
+            }
+            case 404:
+            case 500: {
+                resp.json().then(data => {document.getElementById("error").textContent = data.error});
+                break;
+            }
+            default: {
+                console.log(resp.status);
+            }
+        }
     })
     .catch( error => console.error(error) );
