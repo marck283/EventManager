86380312c6d5747733f1405060a45231b579c8ef
enrico-fornasa <enrico.fornasa@studenti.unitn.it>
2022-05-19 20:52:34 +0200

    Terminato profilo utente

--- app/app.js
+++ app/app.js
@@ -4,13 +4,9 @@ const app = express();
 const authentication = require('./authentication.js');
 const autenticato = require('./authentication.js');
 const tokenChecker = require('./tokenChecker.js');
+const infoUtente = require('./infoUtente.js');
 
 const registrato = require('./registrazione.js');
-
-
-
-
-
 
 /**
  * Configure Express.js parsing middleware
@@ -23,29 +19,19 @@ app.use(express.urlencoded({ extended: true }));
  * Serve front-end static files
  */
 
-
-
 app.use('/api/v1/authentications', autenticato);
-
-
 app.use('/', express.static('static'));
+
+app.use('/api/v1/Utenti', infoUtente); //Da cambiare sotto tokenChecker
 
 app.use(tokenChecker);
 
-
-
-
-
-app.use('/api/v1/Utenti',registrato);
-
-
+app.use('/api/v1/Utenti', registrato);
 
 /* Default 404 handler */
 app.use((req, res) => {
     res.status(404);
     res.json({ error: 'Non Trovato' });
 });
-
-
 
 module.exports = app;
