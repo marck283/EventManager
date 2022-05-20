5554d40a00578d8160996157b8708db6f528b29b
GuglielmoZocca <105376442+GuglielmoZocca@users.noreply.github.com>
2022-05-20 16:46:06 +0200

    Delete eventpublic.js

--- app/collezioni/eventpublic.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-
-
-// Si crea un modello per l'evento personale
-
-module.exports = mongoose.model('EventoP', new Schema({ 
-	data: String,  ora: String, durata: Number, maxPers: Number, categoria: String, nomeAtt: String, luogoEv: {indirizzo: String, citta: String}, organizzatoreID: String, partecipantiID: [String]}));
