5b5464a05dea6ce6013a46a66ef19414b174632b
Guglielmo Zocca <guglielmo.zocca@studenti.unitn.it>
2022-05-20 16:15:27 +0200

    frfrf

--- app/collezioni/eventpersonal.js
+++ app/collezioni/eventpersonal.js
@@ -1,9 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
-// Si crea un modello per l'evento personale
-
+// set up a mongoose model
 module.exports = mongoose.model('EventoPe', new Schema({ 
 	data: String,  ora: String, durata: Number, categoria: String, nomeAtt: String, luogoEv: {indirizzo: String, citta: String}, organizzatoreID: String}
 ));
--- app/collezioni/eventpublic.js
+++ app/collezioni/eventpublic.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
-
-// Si crea un modello per l'evento personale
-
+// set up a mongoose model
 module.exports = mongoose.model('EventoP', new Schema({ 
 	data: String,  ora: String, durata: Number, maxPers: Number, categoria: String, nomeAtt: String, luogoEv: {indirizzo: String, citta: String}, organizzatoreID: String, partecipantiID: [String]}));
