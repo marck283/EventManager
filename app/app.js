a4cda53d877facaf612c42ea36a1a2484c264530
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-05-20 16:13:27 +0200

    Reset changes

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
-
-var personalEvents = require("./events/elencoEventiPersonali.js"), personalList = require('./events/listaEventiPersonali.js');
 var express = require('express');
 var path = require('path');
 var app = express();
+const infoEventoPubblico = require('./infoEventiPublic');
+const infoEventoPersonale = require('./infoEventiPersonal');
+
+
+//Si recuperano i router per la gestione della creazione degli eventi
+const eventspublics = require('./eventipubblici.js');
+const eventspersonals = require('./eventiPersonali.js');
+
+var personalEvents = require("./events/elencoEventiPersonali.js"), personalList = require('./events/listaEventiPersonali.js');
+
 const eventList = require("./events/listaEventiPublic.js"), calendarEvents = require("./events/elencoEventiPublic.js");
 
 const autenticato = require('./authentication.js');
@@ -11,12 +19,12 @@ const infoUtente = require('./infoUtente.js');
 
 const registrato = require('./registrazione.js');
 
+
 /**
  * Configure Express.js parsing middleware
  */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 /**
  * Serve front-end static files
@@ -24,6 +32,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/authentications', autenticato);
 app.use('/', express.static('static'));
+
+app.use('/api/v1/EventiPubblici', infoEventoPubblico);
+app.use('/api/v1/EventiPersonali', infoEventoPersonale);
 
 app.use('/api/v1/Utenti', infoUtente); //Da cambiare sotto tokenChecker
 
@@ -35,6 +46,11 @@ app.use("/api/v1/EventiPubblici", eventList);
 app.use("/api/v1/GiorniCalendarioPubblico", calendarEvents);
 
 app.use(tokenChecker);
+
+//Si posizionano i middleware pre la gestione della creazione degli eventi
+app.use('/api/v1/EventiPubblici', eventspublics);
+app.use('/api/v1/EventiPersonali', eventspersonals);
+
 
 /* Default 404 handler */
 app.use((req, res) => {
---
+++ static/scripts/richiesta_creazioneEvento.js
@@ -0,0 +1,292 @@
+var requestPu = () => { //funzione che mi permette di fare i vari controlli delle info per creare un certo evento pubblico date 
+    //in input nella pagina e poi mi permette di fare un richiesta ajax al server. La risposta la gestisco stampandomi il percorso per quella risorsa
+    var inviare = true
+    if(ElencoDate==""){
+        document.getElementById("vuotoDa").innerHTML="inserire una data";
+        inviare=false;
+    }else{
+        document.getElementById("vuotoDa").innerHTML="";
+    }
+    if(document.getElementById("ora").value==""){
+
+        document.getElementById("vuotoO").innerHTML="inserire Ora";
+        inviare=false;
+    }else{
+        var reg= /([0-9]+):([0-9]+)/
+        if(reg.test(document.getElementById("ora").value)){
+            strin=document.getElementById("ora").value.split(":");
+            if(strin.length>2){
+                document.getElementById("vuotoO").innerHTML="formato ora: hh:mm";
+                inviare=false;
+            }else{
+                if(strin[0].length==2 && strin[1].length==2) {
+                    str1 = strin[0];
+                    str2 = strin[1];
+                    if(strin[0][0]==0){
+                        str1=strin[0][1];
+
+                    }
+                    if(strin[1][0]==0){
+                        str2=strin[1][1];
+                    }
+                    if(Number(str1)<=23 && Number(str1)>=0 && Number(str2)<=59 && Number(str2)>=0){
+                        var d = new Date()
+                    
+                        if(Number(str1) >= d.getHours()){
+                            
+                         
+                            if(Number(str1) == d.getHours()){
+
+                                
+
+                                if(Number(str2) >= d.getMinutes()){
+
+                                        document.getElementById("vuotoO").innerHTML="";
+
+
+                                }else{
+                                    document.getElementById("vuotoO").innerHTML="Inserisci orario corretto";
+                                    inviare=false;
+
+                                }
+
+
+                            }else{
+
+                                document.getElementById("vuotoO").innerHTML="";
+
+                            }
+
+
+                        }else{
+                            document.getElementById("vuotoO").innerHTML="Inserisci orario corretto";
+                            inviare=false;
+
+                        }
+     
+
+                    }else{
+                        document.getElementById("vuotoO").innerHTML="formato ora: hh:mm";
+                        inviare=false;
+                    }
+
+
+                    
+                }else{
+                    document.getElementById("vuotoO").innerHTML="formato ora: hh:mm";
+                    inviare=false;
+                }
+            }
+            
+
+        }else{
+            document.getElementById("vuotoO").innerHTML="formato ora: hh:mm";
+            inviare=false;
+        }
+        
+    }
+    if(document.getElementById("nomeAtt").value==""){
+        document.getElementById("vuotoN").innerHTML="inserire nome attività";
+        inviare=false;
+    }else{
+        document.getElementById("vuotoN").innerHTML="";
+    }
+    if(document.getElementById("indirizzo").value==""){
+        document.getElementById("vuotoI").innerHTML="inserire indirizzo";
+        inviare=false;
+    }else{
+        document.getElementById("vuotoI").innerHTML="";
+    }
+    if(document.getElementById("Citta").value==""){
+        document.getElementById("vuotoCi").innerHTML="inserire città";
+        inviare=false;
+    }else{
+        document.getElementById("vuotoCi").innerHTML="";
+    }
+    if(inviare==true){
+
+        token = "djfbjhekjhf"
+        var reqObj = new XMLHttpRequest(), eventJSONList;
+        reqObj.open("Post", "/api/v1/EventiPubblici", true); //Invio una richiesta asincrona al server Node.js
+        reqObj.setRequestHeader('Content-Type', 'application/json');
+        reqObj.setRequestHeader('x-access-token', token);
+        //reqObj.responseType = "json";
+        reqObj.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 201) {
+                console.log("ricevuto")
+                //Popola la pagina con i dati ricevuti
+                eventJSONList = this.response; //Cattura la risposta in formato JSON
+                locazione=this.getResponseHeader("Location")
+                document.getElementById("loc").innerHTML = locazione
+                
+            }
+            if (this.readyState === 4 && this.status === 500) {
+                console.log("ricevuto")
+                //Popola la pagina con i dati ricevuti
+                eventJSONList = this.response; //Cattura la risposta in formato JSON
+                locazione=this.getResponseHeader("Location")
+                document.getElementById("loc").innerHTML = eventJSONList.error
+                
+            }
+            if (this.readyState === 4 && this.status === 404) {
+                console.log("ricevuto")
+                //Popola la pagina con i dati ricevuti
+                eventJSONList = this.response; //Cattura la risposta in formato JSON
+                locazione=this.getResponseHeader("Location")
+                document.getElementById("loc").innerHTML = eventJSONList.error
+                
+            }
+    };
+    
+
+    
+        reqObj.send(JSON.stringify({ "data": ElencoDate, "ora": document.getElementById("ora").value, "durata": document.getElementById("durata").value, "maxPers": document.getElementById("maxPers").value,"categoria": document.getElementById("categoria").value, "nomeAtt": document.getElementById("nomeAtt").value, "luogoEv": {"indirizzo": document.getElementById("indirizzo").value, "citta": document.getElementById("Citta").value}}))
+    }   
+
+};
+
+//funzione che mi permette di fare i vari controlli delle info per creare un certo evento personale date 
+//in input nella pagina e poi mi permette di fare un richiesta ajax al server. La risposta la gestisco stampandomi il percorso per quella risorsa
+
+var requestPe = () => {
+    //reqObj.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    var inviare = true
+    if(ElencoDate==""){
+        document.getElementById("vuotoDa").innerHTML="inserire una data";
+        inviare=false;
+    }else{
+        document.getElementById("vuotoDa").innerHTML="";
+    }
+    if(document.getElementById("ora").value==""){
+
+        document.getElementById("vuotoO").innerHTML="inserire Ora";
+        inviare=false;
+    }else{
+        var reg= /([0-9]+):([0-9]+)/
+        if(reg.test(document.getElementById("ora").value)){
+            strin=document.getElementById("ora").value.split(":");
+            if(strin.length>2){
+                document.getElementById("vuotoO").innerHTML="formato ora: hh:mm";
+                inviare=false;
+            }else{
+                if(strin[0].length==2 && strin[1].length==2) {
+                    str1 = strin[0];
+                    str2 = strin[1];
+                    if(strin[0][0]==0){
+                        str1=strin[0][1];
+
+                    }
+                    if(strin[1][0]==0){
+                        str2=strin[1][1];
+                    }
+                    if(Number(str1)<=23 && Number(str1)>=0 && Number(str2)<=59 && Number(str2)>=0){
+                        var d = new Date()
+                    
+                        if(Number(str1) >= d.getHours()){
+                            
+                         
+                            if(Number(str1) == d.getHours()){
+
+                                
+
+                                if(Number(str2) >= d.getMinutes()){
+
+                                        document.getElementById("vuotoO").innerHTML="";
+
+
+                                }else{
+                                    document.getElementById("vuotoO").innerHTML="Inserisci orario corretto";
+                                    inviare=false;
+
+                                }
+
+
+                            }else{
+
+                                document.getElementById("vuotoO").innerHTML="";
+
+                            }
+
+
+                        }else{
+                            document.getElementById("vuotoO").innerHTML="Inserisci orario corretto";
+                            inviare=false;
+
+                        }
+
+                    }else{
+                        document.getElementById("vuotoO").innerHTML="formato ora: hh:mm";
+                        inviare=false;
+                    }
+                    
+                }else{
+                    document.getElementById("vuotoO").innerHTML="formato ora: hh:mm";
+                    inviare=false;
+                }
+            }
+            
+
+        }else{
+            document.getElementById("vuotoO").innerHTML="formato ora: hh:mm";
+            inviare=false;
+        }
+        
+    }
+    if(document.getElementById("nomeAtt").value==""){
+        document.getElementById("vuotoN").innerHTML="inserire nome attività";
+        inviare=false;
+    }else{
+        document.getElementById("vuotoN").innerHTML="";
+    }
+    if(document.getElementById("indirizzo").value==""){
+        document.getElementById("vuotoI").innerHTML="inserire indirizzo";
+        inviare=false;
+    }else{
+        document.getElementById("vuotoI").innerHTML="";
+    }
+    if(document.getElementById("Citta").value==""){
+        document.getElementById("vuotoCi").innerHTML="inserire città";
+        inviare=false;
+    }else{
+        document.getElementById("vuotoCi").innerHTML="";
+    }
+    if(inviare==true){
+        token = "djfbjhekjhf"
+        var reqObj = new XMLHttpRequest(), eventJSONList;
+        reqObj.open("Post", "/api/v1/EventiPersonali", true); //Invio una richiesta asincrona al server Node.js
+        reqObj.setRequestHeader('Content-Type', 'application/json');
+        reqObj.setRequestHeader('x-access-token', token);
+        //reqObj.responseType = "json";
+        reqObj.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 201) {
+                console.log("ricevuto")
+                //Popola la pagina con i dati ricevuti
+                eventJSONList = this.response; //Cattura la risposta in formato JSON
+                locazione=this.getResponseHeader("Location")
+                document.getElementById("loc").innerHTML = locazione
+                
+            }
+            if (this.readyState === 4 && this.status === 500) {
+                console.log("ricevuto")
+                //Popola la pagina con i dati ricevuti
+                eventJSONList = this.response; //Cattura la risposta in formato JSON
+                locazione=this.getResponseHeader("Location")
+                document.getElementById("loc").innerHTML = eventJSONList.error
+                
+            }
+            if (this.readyState === 4 && this.status === 404) {
+                console.log("ricevuto")
+                //Popola la pagina con i dati ricevuti
+                eventJSONList = this.response; //Cattura la risposta in formato JSON
+                locazione=this.getResponseHeader("Location")
+                document.getElementById("loc").innerHTML = eventJSONList.error
+                
+            }
+    };
+        
+    
+        reqObj.send(JSON.stringify({ "data": ElencoDate, "ora": document.getElementById("ora").value, "durata": document.getElementById("durata").value,"categoria": document.getElementById("categoria").value, "nomeAtt": document.getElementById("nomeAtt").value, "luogoEv": {"indirizzo": document.getElementById("indirizzo").value, "citta": document.getElementById("Citta").value}}))
+    }   
+
+};
+
