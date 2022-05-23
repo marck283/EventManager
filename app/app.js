85cedd8ffe3183c2ca83074ce49976e67992355f
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-05-23 21:38:08 +0200

    Second commit after pull

--- app/app.js
+++ app/app.js
@@ -2,28 +2,11 @@ var express = require('express');
 var path = require('path');
 const cors = require('cors');
 var app = express();
-<<<<<<< HEAD
-const infoEventoPubblico = require('./infoEventiPublic');
-const infoEventoPersonale = require('./infoEventiPersonal');
-
-
-
-//Si recuperano i router per la gestione della creazione degli eventi
-const eventspublics = require('./eventipubblici.js');
-const eventspersonals = require('./eventiPersonali.js');
-
-var personalEvents = require("./events/elencoEventiPersonali.js"), personalList = require('./events/listaEventiPersonali.js');
-
-
-const eventList = require("./events/listaEventiPublic.js"), calendarEvents = require("./events/elencoEventiPublic.js");
-
-=======
 const EventoPubblico = require('./events/EventiPub.js');
 const EventoPubIscrCrea = require('./events/IscrCreEvenPub.js');
 const EventoPersonale = require('./events/EventiPers.js');
 const calendarEventsPers = require("./events/elencoEventiPersonali.js")
 const calendarEventsPub = require("./events/elencoEventiPublic.js");
->>>>>>> 020c74390c6f108be5711910831143d0fbfea128
 const autenticato = require('./authentication.js');
 const tokenChecker = require('./tokenChecker.js');
 const Utente = require('./Utenti.js');
@@ -38,21 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-<<<<<<< HEAD
-/**
- * Serve front-end static files
- */
-
-app.use('/api/v1/authentications', autenticato);
-
-
-=======
 app.use(cors());
 
 /**
  * Serve front-end static files
  */
->>>>>>> 020c74390c6f108be5711910831143d0fbfea128
 app.use('/', express.static('static'));
 app.use('/api/v1/authentications', autenticato);
 app.use('/api/v1/EventiPubblici', EventoPubblico);
@@ -62,18 +35,11 @@ app.use('/api/v1/Utenti', registrato);
 app.use('/api/v1/Utenti',registrato);
 app.use(tokenChecker);
 
-<<<<<<< HEAD
-
-//Si posizionano i middleware pre la gestione della creazione degli eventi
-app.use('/api/v1/EventiPubblici', eventspublics);
-app.use('/api/v1/EventiPersonali', eventspersonals);
-=======
 //********************************************************** attenzione *********************
 app.use('/api/v1/EventiPubblici', EventoPubIscrCrea);
 app.use('/api/v1/EventiPersonali', EventoPersonale);
 app.use('/api/v1/Utenti', Utente);
 app.use("/api/v1/eventiCalendarioPersonale", calendarEventsPers);
->>>>>>> 020c74390c6f108be5711910831143d0fbfea128
 
 
 
--- app/events/EventiPub.js
+++ app/events/EventiPub.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD:app/infoEventiPublic.js
-const express = require('express');
-const router = express.Router();
-const EventPub = require('./collezioni/eventPublic');
-const Users = require('./collezioni/utenti');
-
-router.get('/:id', async(req, res) => {
-
-    try{
-        let eventoPubblico = await EventPub.findById(req.params.id);
-        let organizzatore = await Users.findById(eventoPubblico.organizzatoreID);
-        let partecipanti = [];
-        for (var i of eventoPubblico.partecipantiID){
-            let tmp = await Users.findById(i);
-            partecipanti.push(tmp.nome);
-        }
-
-        res.status(200).json({
-            nomeAtt: eventoPubblico.nomeAtt,
-            categoria: eventoPubblico.categoria,
-            data: eventoPubblico.data,
-            ora: eventoPubblico.ora,
-            durata: eventoPubblico.durata,
-            luogoEv: eventoPubblico.luogoEv,
-            organizzatore: organizzatore.nome,
-            maxPers: eventoPubblico.maxPers,
-            partecipanti: partecipanti
-        });
-    }catch(error){
-        console.log(error);
-        res.status(500).json({error: "Errore nel Server"}).send();
-    }
-
-});
-
-=======
 const express = require('express');
 const eventPublic = require('../collezioni/eventPublic.js');
 const router = express.Router();
@@ -74,5 +38,4 @@ router.get('/:id', async(req, res) => {
 
 
 
->>>>>>> 020c74390c6f108be5711910831143d0fbfea128:app/events/EventiPub.js
 module.exports = router;
