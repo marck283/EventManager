e9e0edd6aa74010102d82b33768d389bcf90b4c1
Guglielmo Zocca <guglielmo.zocca@studenti.unitn.it>
2022-05-27 21:07:22 +0200

    prima parte visualizzazione inviti

--- app/Utenti.js
+++ app/Utenti.js
@@ -25,6 +25,26 @@ router.get('/me', async (req, res) => {
 });
 
 
+router.get('/me/Inviti', async (req, res) => {
+
+    IDexample = req.loggedUser.id;
+    
+    try{
+        let utente = await Utente.findById(IDexample);
+
+        res.status(200).json({
+            nome: utente.nome,
+            email: utente.email,
+            tel: utente.tel,
+            password: utente.password,
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error: "Errore nel Server"}).send();
+    }
+});
+
+
 
 
 module.exports=router;
