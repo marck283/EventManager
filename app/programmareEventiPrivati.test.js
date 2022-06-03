82b0aaaca9cabc6cb3f533ba182d17928ad6b189
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-06-03 17:30:59 +0200

    Aggiunta suite di test per la creazione degli eventi privati

--- app/programmareEventiPrivati.test.js
+++ app/programmareEventiPrivati.test.js
@@ -2,7 +2,7 @@ const request = require('supertest'), mongoose = require('mongoose');
 const app = require('./app.js');
 const jwt = require('jsonwebtoken');
 
-module.exports = describe("GET /api/v2/EventiPersonali", () => {
+module.exports = describe("GET /api/v2/EventiPrivati", () => {
     beforeAll(async () => {
         jest.setTimeout(8000);
         app.locals.db = await mongoose.connect(process.env.DB_URL_TEST);
@@ -18,302 +18,318 @@ module.exports = describe("GET /api/v2/EventiPersonali", () => {
         { expiresIn: 86400 }
     );
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e tutti i campi obbligatori compilati correttamente", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e tutti i campi obbligatori compilati correttamente", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/27/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 5",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(201);
     });
 
-    test("POST /api/v2/EventiPersonali con utente non autenticato", () => {
+    test("POST /api/v2/EventiPrivati con utente non autenticato", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', '')
         .set('Accept', 'application/json')
         .send({
             data: "06/27/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 1",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 1",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(401, {success: false, message: 'fallita autenticazione'});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo durata compilato con valore non numerico", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo durata compilato con valore non numerico", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/27/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 6",
+            categoria: "svago",
+            durata: "2 giorni",
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 2",
-            categoria: "svago",
-            durata: "2 giorni"
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo non del formato corretto"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo durata compilato con valore numerico negativo", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo durata compilato con valore numerico negativo", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/27/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 2",
+            categoria: "svago",
+            durata: -20,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 2",
-            categoria: "svago",
-            durata: -20
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo vuoto o indefinito"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo durata compilato con valore nullo", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo durata compilato con valore nullo", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/27/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 2",
+            categoria: "svago",
+            durata: 0,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 2",
-            categoria: "svago",
-            durata: 0
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo vuoto o indefinito"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'data' non compilato", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'data' non compilato", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             ora: "11:00",
-            luogoEv: {
-                indirizzo: "Via del campo",
-                citta: "Mortara"
-            },
             nomeAtt: "Girare a vuoto 4",
             categoria: "svago",
-            durata: 2
+            durata: 2,
+            luogoEv: {
+                indirizzo: "Via del campo",
+                citta: "Mortara"
+            },
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo vuoto o indefinito"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'data' compilato inserendo più volte la stessa data", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'data' compilato inserendo più volte la stessa data", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/27/2022,07/08/2024,07/09/2023,06/27/2022,07/08/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 4",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 4",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "date ripetute"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'data' compilato inserendo almeno una data non conforme al formato 'mese/gorno/anno'", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'data' compilato inserendo almeno una data non conforme al formato 'mese/gorno/anno'", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "13/06/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 4",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 4",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "formato data non valido"});
     });
 
     //Test case scritto il 3 giugno
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'data' compilato inserendo una data antecedente a quella corrente", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'data' compilato inserendo una data antecedente a quella corrente", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "02/06/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 4",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 4",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(403, {error: "giorno non disponibile"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'indirizzo' non compilato", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'indirizzo' non compilato", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "02/06/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 4",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 4",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo vuoto o indefinito"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'citta' non compilato", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'citta' non compilato", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "02/06/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 4",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
             },
-            nomeAtt: "Girare a vuoto 4",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo vuoto o indefinito"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'indirizzo' compilato come stringa vuota", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'indirizzo' compilato come stringa vuota", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "02/06/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 4",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 4",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo vuoto o indefinito"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'ora' compilato in formato diverso da hh:mm", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'ora' compilato in formato diverso da hh:mm", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/06/2022",
             ora: "11|00",
+            nomeAtt: "Girare a vuoto 4",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 4",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "formato ora non valido"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'ora' compilato con un orario antecedente all'ora corrente per la data corrente", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'ora' compilato con un orario antecedente all'ora corrente per la data corrente", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/03/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 4",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 4",
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(403, {error: "orario non permesso"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'nomeAtt' non compilato", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'nomeAtt' non compilato", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/03/2022",
             ora: "11:00",
+            categoria: "svago",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            categoria: "svago",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo vuoto o indefinito"});
     });
 
-    test("POST /api/v2/EventiPersonali con utente autenticato e campo 'categoria' non compilato", () => {
+    test("POST /api/v2/EventiPrivati con utente autenticato e campo 'categoria' non compilato", () => {
         return request(app)
-        .post('/api/v2/EventiPersonali')
+        .post('/api/v2/EventiPrivati')
         .set('x-access-token', token)
         .set('Accept', 'application/json')
         .send({
             data: "06/03/2022",
             ora: "11:00",
+            nomeAtt: "Girare a vuoto 4",
+            durata: 2,
             luogoEv: {
                 indirizzo: "Via del campo",
                 citta: "Mortara"
             },
-            nomeAtt: "Girare a vuoto 4",
-            durata: 2
+            ElencoEmailInviti: ["gg.aa@gmail.com"]
         })
         .expect(400, {error: "Campo vuoto o indefinito"});
     });
