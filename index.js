fdba98fe2a8b4e04e98e27dbcfef90a69a9baad6
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-08-09 17:32:59 +0200

    Temporarily removed firebase support

--- index.js
+++ index.js
@@ -2,34 +2,11 @@ require('dotenv').config();
 const app = require('./app/app.js');
 const mongoose = require('mongoose');
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAuZtlyfk2C46hNxmSTv9fG3vGYYlDz7yg",
-  authDomain: "eventmanager-cbe2d.firebaseapp.com",
-  projectId: "eventmanager-cbe2d",
-  storageBucket: "eventmanager-cbe2d.appspot.com",
-  messagingSenderId: "666454225517",
-  appId: "1:666454225517:web:721eb8a341dbeedaac8564",
-  measurementId: "G-D19RMY08YF"
-};
-
-// Initialize Firebase
-const appf = initializeApp(firebaseConfig);
-
-
 const port = process.env.PORT || 8080;
-
 
 /**
  * Configure mongoose
  */
-
 app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then ( () => {
     
