c5999103eac92a4357a502cb9d8effffe005b92c
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-08-24 20:10:45 +0200

    Fixed potential DOMXSS vulnerability

--- static/scripts/inviti.js
+++ static/scripts/inviti.js
@@ -1,158 +1,86 @@
-
-
-
 var token = "";
-
-if(localStorage.getItem('token') != null){
-
-    token = localStorage.getItem('token');
+if (localStorage.getItem('token') != null) {
+	token = localStorage.getItem('token');
 }
 
-
-
-
 fetch('../api/v2/Utenti/me/Inviti', {
-		        method: 'GET',
-		        headers: {
-            		'x-access-token': token
-        		}
-		    	}).then((resp) => {
-		    		
-		    		/**if(resp.status==403 || resp.status==400){
-		    			document.getElementById("iscrizione").innerHTML = "non iscritto";
-		    		}
-		    		if(resp.status==201){
-		    			document.getElementById("iscrizione").innerHTML = "Iscritto " + "\n" + resp.headers;
-		    		}
-					*/
-		    		
-        			if(resp.status==200){
-        				
-        				resp.json().then(data => {
+	method: 'GET',
+	headers: {
+		'x-access-token': token
+	}
+}).then(resp => {
+	switch (resp.status) {
+		case 200: {
+			resp.json().then(data => {
+				let paragrafo = document.getElementById("inviti");
 
+				for (elem of data) {
+					let d = document.createElement("div");
 
-        					let paragrafo = document.getElementById("inviti");
+					let tipo = document.createElement("h4");
+					tipo.textContent = "Tipo Evento: ";
+					switch (elem.tipoevento) {
+						case "pub": {
+							tipo.textContent += "pubblico";
+							break;
+						}
+						case "priv": {
+							tipo.textContent += "privato";
+							break;
+						}
+						default: {
+							tipo.textContent += "tipo evento non riconosciuto";
+							break;
+						}
+					}
 
-        					for(elem of data){
-        						
+					d.appendChild(tipo);
 
+					let h1 = document.createElement("h4");
+					h1.textContent = "Evento: " + elem.nomeAtt;
 
-		                        let d = document.createElement("div");
+					d.appendChild(h1);
 
-		                        let tipo = document.createElement("h4");
+					let h2 = document.createElement("h4");
+					h2.textContent = "Organizzatore: " + elem.nomeOrg;
 
-		                        tipo.innerHTML = "Tipo Evento: ";
-								switch(elem.tipoevento) {
-									case "pub": {
-										tipo.innerHTML += "pubblico";
-										break;
-									}
-									case "priv": {
-										tipo.innerHTML += "privato";
-										break;
-									}
-									default: {
-										tipo.innerHTML += "tipo evento non riconosciuto";
-										break;
-									}
-								}
+					d.appendChild(h2);
 
-		                        d.appendChild(tipo);
+					var h4 = document.createElement("h4");
+					if (elem.accettato) {
+						h4.textContent = "già accettato";
+						d.appendChild(h4);
+					}
 
-		                        let h1 = document.createElement("h4");
+					let pulsante = document.createElement("button");
+					pulsante.textContent = "Iscrizione";
+					pulsante.classList = "btn btn-primary";
 
-		                        h1.innerHTML = "Evento: " + elem.nomeAtt;
+					if (elem.tipoevento == "pub") {
+						pulsante.onclick = iscrInv.bind(this, elem.idevento);
+					} else {
+						pulsante.onclick = iscrPr.bind(this, elem.idevento);
+					}
 
-		                        d.appendChild(h1);
+					d.appendChild(pulsante);
 
-		                        let h2 = document.createElement("h4");
+					let h3 = document.createElement("h4");
+					h3.textContent = "--------------------"
+					d.appendChild(h3);
 
-		                        h2.innerHTML = "Organizzatore: " + elem.nomeOrg;
-
-		                        d.appendChild(h2);
-
-		                        var h4 = document.createElement("h4");
-
-		                      
-
-		                        if(elem.accettato == true){
-
-		                        	
-
-		                        	h4.innerHTML = "già accettato"
-
-		                        	d.appendChild(h4);
-
-
-		                        }
-
-
-
-		                        let pulsante = document.createElement("button");
-		                        pulsante.textContent = "Iscrizione";
-								pulsante.classList = "btn btn-primary";
-
-		                        if(elem.tipoevento == "pub"){
-		                            
-
-		                            pulsante.onclick = iscrInv.bind(this,elem.idevento);
-		                        
-
-		                        }else{
-
-		                            pulsante.onclick = iscrPr.bind(this,elem.idevento);
-
-
-		                        }
-
-		                        d.appendChild(pulsante)
-
-
-		                        let h3 = document.createElement("h4");
-
-		                        h3.innerHTML = "--------------------"
-
-
-		                        d.appendChild(h3);
-
-		                        
-
-		                        paragrafo.appendChild(d)
-
-		                    	
-		                    	
-		                        
-
-		                    }
-
-
-
-        				});
-
-
-        			}
-        			if(resp.status==403){
-        				resp.json().then(data => {document.getElementById("error").innerHTML = data.error});
-        				
-        				
-
-        			}
-        			if(resp.status==500){
-        				resp.json().then(data => {document.getElementById("error").innerHTML = data.error});
-
-        			}
-        			if(resp.status==404){
-        				resp.json().then(data => {document.getElementById("error").innerHTML = data.error});
-
-        			}
-        			if(resp.status==401){
-        				resp.json().then(data => {document.getElementById("error").innerHTML = data.message});
-
-        			}
-
-        			return;
-    			}).catch( error => console.error(error) ); // If there is any error you will catch them here
-
-		
-
-
+					paragrafo.appendChild(d);
+				}
+			});
+		}
+		case 401: {
+			resp.json().then(data => { document.getElementById("error").textContent = data.message });
+		}
+		case 403:
+		case 404:
+		case 500: {
+			resp.json().then(data => document.getElementById("error").textContent = data.error);
+			break;
+		}
+	}
+	return;
+}).catch(error => console.error(error)); // If there is any error you will catch them here
