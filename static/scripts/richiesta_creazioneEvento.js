a0b2bc7e115a4811f2ebdadda6244b155438468a
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2022-09-13 15:38:19 +0200

    Rewritten code to take out some unuseful lines

--- static/scripts/richiesta_creazioneEvento.js
+++ static/scripts/richiesta_creazioneEvento.js
@@ -1,61 +1,44 @@
 var checkDateArr = (dateArr, temp_poz, d, str1, str2) => {
-    if (dateArr.includes(temp_poz)) {
-        if (Number(str1) < d.getHours() || (Number(str1) == d.getHours() && Number(str2) < d.getMinutes())) {
-            document.getElementById("vuotoO").textContent = "Inserisci orario corretto";
-            return true;
-        } else {
-            document.getElementById("vuotoO").textContent = "";
-        }
+    if (dateArr.includes(temp_poz) && (Number(str1) < d.getHours() || (Number(str1) == d.getHours() && Number(str2) < d.getMinutes()))) {
+        alert("Inserisci orario corretto.");
+        return true;
     }
     return false;
 };
 
-var checkSendCondition = (condition, fieldName, errorText) => {
+var checkSendCondition = (condition, errorText) => {
     if (condition) {
-        document.getElementById(fieldName).textContent = errorText;
+        alert(errorText);
         return false;
-    } else {
-        document.getElementById(fieldName).textContent = "";
     }
     return true;
 }
 
 var requestPu = () => { //funzione che mi permette di fare i vari controlli delle info per creare un certo evento pubblico date 
     //in input nella pagina e poi mi permette di fare un richiesta al server. La risposta la gestisco stampandomi il percorso per quella risorsa
-    var inviare = checkSendCondition(ElencoDate == "", "vuotoDa", "Inserire una data");
+    var inviare = checkSendCondition(ElencoDate == "", "Inserire una data"), reg = /^(((0|1)[0-9])|2[0-3]):[0-5][0-9]$/;
     if (inviare) {
-        if (document.getElementById("ora").value == "") {
-            document.getElementById("vuotoO").textContent = "Inserire un orario nel formato ore:minuti";
+        if (getId("ora").value == "" || !reg.test(getId("ora").value)) {
+            alert("Inserire un orario nel formato ore:minuti");
             inviare = false;
         } else {
-            var reg = /^(((0|1)[0-9])|2[0-3]):[0-5][0-9]$/;
-            if (reg.test(document.getElementById("ora").value)) {
-                var str = document.getElementById("ora").value.split(":"), str1 = str[0], str2 = str[1];
-                var d = new Date();
+            var str = getId("ora").value.split(":"), str1 = str[0], str2 = str[1], d = new Date();
 
-                if (ElencoDate != "") {
-                    var mm = d.getMonth() + 1;
-                    var dd = d.getDate();
-                    var yy = d.getFullYear();
+            if (ElencoDate != "") {
+                var mm = d.getMonth() + 1, dd = d.getDate(), yy = d.getFullYear();
+                var giorno = String(dd).padStart(2, '0'), mese = String(mm).padStart(2, '0'), anno = "" + yy;
+                var temp_poz = mese + '/' + giorno + '/' + anno;
 
-                    var giorno = String(dd).padStart(2, '0'), mese = String(mm).padStart(2, '0'), anno = "" + yy;
-
-                    var temp_poz = mese + '/' + giorno + '/' + anno;
-
-                    (checkDateArr(ElencoDate, temp_poz, d, str1, str2)) ? inviare = false : inviare = true;
-                }
-            } else {
-                document.getElementById("vuotoO").textContent = "Inserire un orario nel formato ore:minuti";
-                inviare = false;
+                (checkDateArr(ElencoDate, temp_poz, d, str1, str2)) ? inviare = false : inviare = true;
             }
         }
     }
 
-    inviare = checkSendCondition(document.getElementById("nomeAtt").value == "", "vuotoN", "Inserire nome attivit&agrave;") ||
-        checkSendCondition(document.getElementById("indirizzo").value == "", "vuotoI", "Inserire indirizzo") ||
-        checkSendCondition(document.getElementById("Citta").value == "", "vuotoCi", "Inserire citt&agrave;") ||
-        checkSendCondition(document.getElementById("durata").value == "" || Number.isNaN(parseInt(document.getElementById("durata").value)) || Number(document.getElementById("durata").value) <= 0, "vuotoDu", "Inserire durata corretta") ||
-        checkSendCondition(document.getElementById("maxPers").value == "" || Number.isNaN(parseInt(document.getElementById("maxPers").value)) || Number(document.getElementById("maxPers").value) < 2, "vuotoM", "Inserire numero massimo persone corretto");
+    inviare = checkSendCondition(getId("nomeAtt").value == "", "Inserire nome attivit&agrave;") ||
+        checkSendCondition(getId("indirizzo").value == "", "Inserire indirizzo") ||
+        checkSendCondition(getId("Citta").value == "", "Inserire citt&agrave;") ||
+        checkSendCondition(getId("durata").value == "" || Number.isNaN(parseInt(getId("durata").value)) || Number(getId("durata").value) <= 0, "Inserire durata corretta") ||
+        checkSendCondition(getId("maxPers").value == "" || Number.isNaN(parseInt(getId("maxPers").value)) || Number(getId("maxPers").value) < 2, "Inserire numero massimo persone corretto");
 
     if (inviare) {
         var Token = token, eventJSONList;
@@ -67,14 +50,14 @@ var requestPu = () => { //funzione che mi permette di fare i vari controlli dell
             },
             body: JSON.stringify({
                 data: ElencoDate,
-                ora: document.getElementById("ora").value,
-                durata: Number(document.getElementById("durata").value),
-                maxPers: Number(document.getElementById("maxPers").value),
-                categoria: document.getElementById("categoria").value,
-                nomeAtt: document.getElementById("nomeAtt").value,
+                ora: getId("ora").value,
+                durata: Number(getId("durata").value),
+                maxPers: Number(getId("maxPers").value),
+                categoria: getId("categoria").value,
+                nomeAtt: getId("nomeAtt").value,
                 luogoEv: {
-                    indirizzo: document.getElementById("indirizzo").value,
-                    citta: document.getElementById("Citta").value
+                    indirizzo: getId("indirizzo").value,
+                    citta: getId("Citta").value
                 }
             })
         })
@@ -123,13 +106,13 @@ var requestPu = () => { //funzione che mi permette di fare i vari controlli dell
 var requestPe = () => {
     //reqObj.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     var inviare = checkSendCondition(ElencoDate == "", "vuotoDa", "Inserire una data");
-    if (document.getElementById("ora").value == "") {
-        document.getElementById("vuotoO").textContent = "inserire Ora";
+    if (getId("ora").value == "") {
+        getId("vuotoO").textContent = "inserire Ora";
         inviare = false;
     } else {
         var reg = /^(((0|1)[0-9])|2[0-3]):[0-5][0-9]$/;
-        if (reg.test(document.getElementById("ora").value)) {
-            strin = document.getElementById("ora").value.split(":");
+        if (reg.test(getId("ora").value)) {
+            strin = getId("ora").value.split(":");
             str1 = strin[0];
             str2 = strin[1];
             var d = new Date();
@@ -146,33 +129,33 @@ var requestPe = () => {
                 (checkDateArr(ElencoDate, temp_poz, d, str1, str2)) ? inviare = false : inviare = true;
             }
         } else {
-            document.getElementById("vuotoO").textContent = "formato ora: hh:mm";
+            getId("vuotoO").textContent = "formato ora: hh:mm";
             inviare = false;
         }
     }
-    if (document.getElementById("nomeAtt").value == "") {
-        document.getElementById("vuotoN").textContent = "inserire nome attività";
+    if (getId("nomeAtt").value == "") {
+        getId("vuotoN").textContent = "inserire nome attività";
         inviare = false;
     } else {
-        document.getElementById("vuotoN").textContent = "";
+        getId("vuotoN").textContent = "";
     }
-    if (document.getElementById("indirizzo").value == "") {
-        document.getElementById("vuotoI").textContent = "inserire indirizzo";
+    if (getId("indirizzo").value == "") {
+        getId("vuotoI").textContent = "inserire indirizzo";
         inviare = false;
     } else {
-        document.getElementById("vuotoI").textContent = "";
+        getId("vuotoI").textContent = "";
     }
-    if (document.getElementById("Citta").value == "") {
-        document.getElementById("vuotoCi").textContent = "inserire città";
+    if (getId("Citta").value == "") {
+        getId("vuotoCi").textContent = "inserire città";
         inviare = false;
     } else {
-        document.getElementById("vuotoCi").textContent = "";
+        getId("vuotoCi").textContent = "";
     }
-    if (document.getElementById("durata").value == "" || Number.isNaN(parseInt(document.getElementById("durata").value)) || Number(document.getElementById("durata").value) <= 0) {
-        document.getElementById("vuotoDu").textContent = "inserire durata corretta";
+    if (getId("durata").value == "" || Number.isNaN(parseInt(getId("durata").value)) || Number(getId("durata").value) <= 0) {
+        getId("vuotoDu").textContent = "inserire durata corretta";
         inviare = false;
     } else {
-        document.getElementById("vuotoDu").textContent = "";
+        getId("vuotoDu").textContent = "";
     }
     if (inviare) {
         var Token = token, eventJSONList;
@@ -184,13 +167,13 @@ var requestPe = () => {
             },
             body: JSON.stringify({
                 data: ElencoDate,
-                ora: document.getElementById("ora").value,
-                durata: Number(document.getElementById("durata").value),
-                categoria: document.getElementById("categoria").value,
-                nomeAtt: document.getElementById("nomeAtt").value,
+                ora: getId("ora").value,
+                durata: Number(getId("durata").value),
+                categoria: getId("categoria").value,
+                nomeAtt: getId("nomeAtt").value,
                 luogoEv: {
-                    indirizzo: document.getElementById("indirizzo").value,
-                    citta: document.getElementById("Citta").value
+                    indirizzo: getId("indirizzo").value,
+                    citta: getId("Citta").value
                 }
             })
         })
@@ -237,19 +220,19 @@ var requestPr = () => {
 
     //Non si potrebbe andare avanti a semplificare come sopra?
     if (ElencoDate == "") {
-        document.getElementById("vuotoDa").textContent = "Inserire una data";
+        getId("vuotoDa").textContent = "Inserire una data";
         inviare = false;
     } else {
-        document.getElementById("vuotoDa").textContent = "";
+        getId("vuotoDa").textContent = "";
     }
-    if (document.getElementById("ora").value == "") {
+    if (getId("ora").value == "") {
 
-        document.getElementById("vuotoO").textContent = "inserire Ora";
+        getId("vuotoO").textContent = "inserire Ora";
         inviare = false;
     } else {
         var reg = /^(((0|1)[0-9])|2[0-3]):[0-5][0-9]$/;
-        if (reg.test(document.getElementById("ora").value)) {
-            strin = document.getElementById("ora").value.split(":");
+        if (reg.test(getId("ora").value)) {
+            strin = getId("ora").value.split(":");
             str1 = strin[0];
             str2 = strin[1];
             var d = new Date();
@@ -266,33 +249,33 @@ var requestPr = () => {
                 (checkDateArr(ElencoDate, temp_poz, d, str1, str2)) ? inviare = false : inviare = true;
             }
         } else {
-            document.getElementById("vuotoO").textContent = "formato ora: hh:mm";
+            getId("vuotoO").textContent = "formato ora: hh:mm";
             inviare = false;
         }
     }
-    if (document.getElementById("nomeAtt").value == "") {
-        document.getElementById("vuotoN").textContent = "inserire nome attività";
+    if (getId("nomeAtt").value == "") {
+        getId("vuotoN").textContent = "inserire nome attività";
         inviare = false;
     } else {
-        document.getElementById("vuotoN").textContent = "";
+        getId("vuotoN").textContent = "";
     }
-    if (document.getElementById("indirizzo").value == "") {
-        document.getElementById("vuotoI").textContent = "inserire indirizzo";
+    if (getId("indirizzo").value == "") {
+        getId("vuotoI").textContent = "inserire indirizzo";
         inviare = false;
     } else {
-        document.getElementById("vuotoI").textContent = "";
+        getId("vuotoI").textContent = "";
     }
-    if (document.getElementById("Citta").value == "") {
-        document.getElementById("vuotoCi").textContent = "inserire città";
+    if (getId("Citta").value == "") {
+        getId("vuotoCi").textContent = "inserire città";
         inviare = false;
     } else {
-        document.getElementById("vuotoCi").textContent = "";
+        getId("vuotoCi").textContent = "";
     }
-    if (document.getElementById("durata").value == "" || Number.isNaN(parseInt(document.getElementById("durata").value)) || Number(document.getElementById("durata").value) <= 0) {
-        document.getElementById("vuotoDu").textContent = "inserire durata corretta";
+    if (getId("durata").value == "" || Number.isNaN(parseInt(getId("durata").value)) || Number(getId("durata").value) <= 0) {
+        getId("vuotoDu").textContent = "inserire durata corretta";
         inviare = false;
     } else {
-        document.getElementById("vuotoDu").textContent = "";
+        getId("vuotoDu").textContent = "";
     }
     if (inviare) {
         //riscrivere richiesta utilizzando la Fetch API
@@ -305,51 +288,51 @@ var requestPr = () => {
             },
             body: JSON.stringify({
                 data: ElencoDate,
-                ora: document.getElementById("ora").value,
-                durata: Number(document.getElementById("durata").value),
-                categoria: document.getElementById("categoria").value,
-                nomeAtt: document.getElementById("nomeAtt").value,
+                ora: getId("ora").value,
+                durata: Number(getId("durata").value),
+                categoria: getId("categoria").value,
+                nomeAtt: getId("nomeAtt").value,
                 luogoEv: {
-                    indirizzo: document.getElementById("indirizzo").value,
-                    citta: document.getElementById("Citta").value
+                    indirizzo: getId("indirizzo").value,
+                    citta: getId("Citta").value
                 },
                 ElencoEmailInviti: guest
             })
         })
-        .then(resp => {
-            switch(resp.status) {
-                case 201: {
-                    eventJSONList = resp;
-                    locazione = resp.headers.get("Location");
-                    alert("Evento privato creato con successo.");
-                    break;
-                }
-                case 500: {
-                    resp.json().then(data => {
-                        eventJSONList = data;
+            .then(resp => {
+                switch (resp.status) {
+                    case 201: {
+                        eventJSONList = resp;
                         locazione = resp.headers.get("Location");
-                        alert(eventJSONList.error);
-                    });
-                    break;
+                        alert("Evento privato creato con successo.");
+                        break;
+                    }
+                    case 500: {
+                        resp.json().then(data => {
+                            eventJSONList = data;
+                            locazione = resp.headers.get("Location");
+                            alert(eventJSONList.error);
+                        });
+                        break;
+                    }
+                    case 404:
+                    case 403:
+                    case 400: {
+                        resp.json().then(data => {
+                            eventJSONList = data;
+                            alert(eventJSONList.error);
+                        });
+                        break;
+                    }
+                    case 401: {
+                        resp.json().then(data => {
+                            eventJSONList = data;
+                            locazione = resp.headers.get("Location");
+                            alert(eventJSONList.message);
+                        });
+                    }
                 }
-                case 404:
-                case 403:
-                case 400: {
-                    resp.json().then(data => {
-                        eventJSONList = data;
-                        alert(eventJSONList.error);
-                    });
-                    break;
-                }
-                case 401: {
-                    resp.json().then(data => {
-                        eventJSONList = data;
-                        locazione = resp.headers.get("Location");
-                        alert(eventJSONList.message);
-                    });
-                }
-            }
-        })
-        .catch(err => console.log(err));
+            })
+            .catch(err => console.log(err));
     }
 };
