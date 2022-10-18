c58efe083fb88949ef594fed9efe56cd19e00256
marck283 <86911602+marck283@users.noreply.github.com>
2022-10-18 23:37:26 +0200

    Added Google Token exchange module #2

--- app/app.js
+++ app/app.js
@@ -17,11 +17,12 @@ const cookieParser = require('cookie-parser');
 const csrfCreation = require('./csrfTokenCreation.js');
 const pubEvOrgList = require('./events/elencoEventiPubOrg.js');
 const recensioni = require('./events/recensioni.js');
+const gTokenExch = require('./googleTokenExchange.js');
 
 /**
  * Configure Express.js parsing middleware
  */
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({limit: '200mb'}));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
@@ -44,6 +45,7 @@ app.use('/api/v2/eventiCalendarioPubblico', calendarEventsPub);
 app.use('/api/v2/Utenti', regandric);
 app.use('/api/v2/RecuperoPassword', recPsw);
 app.use('/api/v2/CsrfToken', csrfCreation);
+app.use('/api/v2/GoogleToken', gTokenExch);
 
 var limiter = RateLimit({
     windowMs: 1*60*1000, //1 minute
