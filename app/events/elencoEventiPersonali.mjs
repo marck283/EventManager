c97374812bf53f04c01f89ab5bf37b987c121433
marck283 <86911602+marck283@users.noreply.github.com>
2022-10-20 13:23:49 +0200

    Fixed bug regarding code used to return past events

--- app/events/elencoEventiPersonali.mjs
+++ app/events/elencoEventiPersonali.mjs
@@ -13,6 +13,10 @@ router.get("/:data", async (req, res) => {
     var obj = {};
     
     var user = req.loggedUser.id || req.loggedUser.sub;
+    if(user === req.loggedUser.sub) {
+        user = await User.findOne({email: {$eq: req.loggedUser.email}});
+        user = user.id;
+    }
 
     //Eseguire la funzione verify, poi cercare gli eventi nel database
     eventsPers = await eventPersonal.find({organizzatoreID: user}); //Richiedi gli eventi personali per la data selezionata.
@@ -38,24 +42,20 @@ router.get("/:data", async (req, res) => {
 
 var findPubEvents = async (user) => {
     var eventsPub = await eventPublic.find({});
-    console.log("Pre-filtering:");
-    console.log(eventsPub);
 
     //Il motivo per vui qui non restituisce nulla nel caso in cui un utente sia autenticato con Google potrebbe essere che,
     //all'iscrizione dell'utente all'evento, non utilizzo il campo "sub" ma l'id di MongoDB?
     //In tal caso, il problema si potrebbe risolvere semplicemente usando il campo "sub" sia qui che per l'iscrizione all'evento...
     eventsPub = eventsPub.filter(e => (e.partecipantiID.includes(user) || e.organizzatoreID == user));
-    console.log("Post-filtering:");
-    console.log(eventsPub);
     return eventsPub;
 };
 
 var filterEvents = (eventsArr, passato) => {
     var curr = new Date();
     if(passato) {
-        return eventsArr.filter(e => new Date(e.data + "T" + e.ora) < curr);
+        return eventsArr.filter(e => new Date(e.data + "Z" + e.ora) < curr);
     }
-    return eventsArr.filter(e => new Date(e.data + "T" + e.ora) >= curr);
+    return eventsArr.filter(e => new Date(e.data + "Z" + e.ora) >= curr);
 };
 
 router.get("", async (req, res) => {
@@ -63,22 +63,19 @@ router.get("", async (req, res) => {
     var user = req.loggedUser.id || req.loggedUser.sub;
     var nomeAtt = req.header("nomeAtt"), categoria = req.header("categoria"), durata = req.header("durata");
     var indirizzo = req.header("indirizzo"), citta = req.header("citta");
-    console.log(user);
+    
     if(user === req.loggedUser.sub) {
         //Se l'utente è autenticato con Google, allora devo prima trovare il documento dell'utente nel database, per poi
         //ottenere l'id di MongoDB e utilizzarlo per cercare gli eventi pubblici a cui l'utente è iscritto.
         user = await User.findOne({email: {$eq: req.loggedUser.email}});
         user = user.id;
     }
-    console.log(user);
 
-    eventsPers = await eventPersonal.find({organizzatoreID: user}).catch(err => console.log(err)); //Richiedi gli eventi personali.
-    console.log("After event find");
-
+    eventsPers = await eventPersonal.find({organizzatoreID: user}); //Richiedi gli eventi personali.
     eventsPub = await findPubEvents(user);
     eventsPriv = await eventPrivate.find({});
     eventsPriv = eventsPriv.filter(e => (e.partecipantiID.find(e => e == user) != undefined || e.organizzatoreID == user));
-    console.log("Before validation...");
+    
     const v = new Validator({
         durata: durata,
         passato: req.query.passato
@@ -92,13 +89,13 @@ router.get("", async (req, res) => {
             res.status(400).json({error: "Richiesta malformata."}).send();
             return;
         }
-        console.log("OK");
+
         if(nomeAtt != undefined && nomeAtt != "") {
             eventsPers = eventsPers.filter(e => e.nomeAtt.includes(nomeAtt));
             eventsPub = eventsPub.filter(e => e.nomeAtt.includes(nomeAtt));
             eventsPriv = eventsPriv.filter(e => e.nomeAtt.includes(nomeAtt));
         }
-    
+        
         if(categoria != undefined && categoria != "") {
             eventsPers = eventsPers.filter(e => e.categoria == categoria);
             eventsPub = eventsPub.filter(e => e.categoria == categoria);
@@ -127,15 +124,14 @@ router.get("", async (req, res) => {
         switch (req.query.passato) {
             case "True": {
                 //Filtro per date passate
+                eventsPers = filterEvents(eventsPers, true);
                 eventsPub = filterEvents(eventsPub, true);
                 eventsPriv = filterEvents(eventsPriv, true);
                 break;
             }
             case "False": {
                 eventsPub = filterEvents(eventsPub, false);
-                console.log(eventsPub);
                 eventsPriv = filterEvents(eventsPriv, false);
-                console.log(eventsPriv);
                 break;
             }
         }
