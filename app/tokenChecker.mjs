66379934869f37211d566b155104e883aa214ecd
marck283 <86911602+marck283@users.noreply.github.com>
2022-10-31 23:54:15 +0100

    Removed print statement

--- app/tokenChecker.mjs
+++ app/tokenChecker.mjs
@@ -5,8 +5,6 @@ import verify from './googleTokenChecker.mjs';
 const tokenChecker = async (req, res, next) => {
 	var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-	console.log(token);
-
 	if (!token || token === "") {
 		res.status(401).send({
 			success: false,
