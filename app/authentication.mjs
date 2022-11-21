ac57faffd848164237fa1e681148380b6c2f3c57
marck283 <86911602+marck283@users.noreply.github.com>
2022-11-21 02:19:28 +0100

    Verified URL parameter

--- app/authentication.mjs
+++ app/authentication.mjs
@@ -173,7 +173,9 @@ router.post("/facebookLogin", async (req, res) => {
 					return;
 				}
 				var error = false;
-				const resp = await fetch(encodeURI("graph.facebook.com/debug_token?input_token=" + req.body.googleJwt + "&access_token=" + process.env.FACEBOOK_MOBILE_TOKEN))
+				var url = new URL("https://graph.facebook.com/debug_token?input_token=" + req.body.googleJwt + "&access_token=" + process.env.FACEBOOK_MOBILE_TOKEN);
+				
+				const resp = await fetch(url)
 					.catch(err => {
 						console.log(err);
 						res.status(500).json({
