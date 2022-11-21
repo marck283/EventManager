7d919c8194f0d20e04643cdecfc8bfe13ecb568e
marck283 <86911602+marck283@users.noreply.github.com>
2022-11-21 23:52:45 +0100

    Modified code

--- app/authentication.mjs
+++ app/authentication.mjs
@@ -190,42 +190,41 @@ router.post("/facebookLogin", async (req, res) => {
 						.then(async json => {
 							console.log(json);
 							if (json.access_token != null && json.access_token != undefined) {
-								const resp = await fetch("https://graph.facebook.com/v15.0/debug_token?input_token=" + json.access_token + "&access_token=" + process.env.FACEBOOK_MOBILE_TOKEN)
+								await fetch("https://graph.facebook.com/v15.0/debug_token?input_token=" + json.access_token + "&access_token=" + process.env.FACEBOOK_MOBILE_TOKEN)
+									.then(async resp => {
+										await resp.json()
+											.then(async json2 => {
+												console.log(json2);
+												await fetch("https://graph.facebook.com/v15.0/" + json2.data.user_id + "?fields=email,name,picture&access_token=" + json.access_token)
+													.then(async resp => {
+														const json1 = await resp.json();
+														var user = new Utente({
+															nome: json1.data.nome,
+															email: json1.data.email,
+															password: "",
+															salt: "",
+															tel: "",
+															profilePic: json.data.picture.data.url,
+															numEvOrg: 0,
+															valutazioneMedia: 0.0,
+															g_refresh_token: ""
+														});
+														await user.save();
+
+														user = await Utente.findOne({ email: { $eq: json1.data.email } });
+														res.status(200).json(result(json.access_token, json1.data.email, user.id, json1.data.picture.data.url)).send();
+													})
+													.catch(err => {
+														res.status(400).json({
+															error: "OAuth exception"
+														}).send();
+													});
+											});
+									})
 									.catch(err => {
 										console.log(err);
 										error = true;
 									});
-
-								if (!error) {
-									await resp.json()
-										.then(async json2 => {
-											console.log(json2);
-											await fetch("https://graph.facebook.com/v15.0/" + json2.data.user_id + "?fields=email,name,picture&access_token=" + json.access_token)
-												.then(async resp => {
-													const json1 = await resp.json();
-													var user = new Utente({
-														nome: json1.data.nome,
-														email: json1.data.email,
-														password: "",
-														salt: "",
-														tel: "",
-														profilePic: json.data.picture.data.url,
-														numEvOrg: 0,
-														valutazioneMedia: 0.0,
-														g_refresh_token: ""
-													});
-													await user.save();
-
-													user = await Utente.findOne({ email: { $eq: json1.data.email } });
-													res.status(200).json(result(json.access_token, json1.data.email, user.id, json1.data.picture.data.url)).send();
-												})
-												.catch(err => {
-													res.status(400).json({
-														error: "OAuth exception"
-													}).send();
-												});
-										});
-								}
 							} else {
 								console.log("Invalid token");
 								res.status(401).json({
