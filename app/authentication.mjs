6c42873cce31e5768417d16a948716d25bb523b3
marck283 <86911602+marck283@users.noreply.github.com>
2022-11-21 01:31:30 +0100

    Added new API route

--- app/authentication.mjs
+++ app/authentication.mjs
@@ -6,7 +6,7 @@ import RateLimit from 'express-rate-limit';
 import { Validator } from 'node-input-validator';
 import verify from './googleTokenChecker.mjs';
 import createToken from './tokenCreation.mjs';
-import {google} from 'googleapis';
+import { google } from 'googleapis';
 
 var limiter = RateLimit({
 	windowMs: 1 * 60 * 1000, //1 minute
@@ -61,7 +61,7 @@ router.post('', (req, res) => {
 			//E QUELLI PER ANDROID HANNO DUE CONTENUTI DIVERSI?
 			if (req.body.googleJwt != null && req.body.googleJwt != undefined) {
 				let gJwt = req.body.googleJwt;
-				if(gJwt.credential != null && gJwt.credential != undefined) {
+				if (gJwt.credential != null && gJwt.credential != undefined) {
 					gJwt = gJwt.credential;
 				}
 
@@ -70,101 +70,51 @@ router.post('', (req, res) => {
 				//check the keys' expiry date in the header of the response of the above link.)
 				//Then follow the instructions in the following link: https://developers.google.com/identity/gsi/web/guides/verify-google-id-token
 				await verify.verify(gJwt)
-				.then(async ticket => {
-					var payload = ticket.getPayload();
-					let user = await Utente.exists({ email: { $eq: payload.email } });
-					if (user == null) {
-						//Create a new user
-						const service = google.people({
-							version: 'v1',
-							auth: process.env.PEOPLE_API_ID,
-							headers: {
-								"Referer": "https://eventmanagerzlf.herokuapp.com/"
+					.then(async ticket => {
+						var payload = ticket.getPayload();
+						let user = await Utente.exists({ email: { $eq: payload.email } });
+						if (user == null) {
+							//Create a new user
+							const service = google.people({
+								version: 'v1',
+								auth: process.env.PEOPLE_API_ID,
+								headers: {
+									"Referer": "https://eventmanagerzlf.herokuapp.com/"
+								}
+							});
+							const res = await service.people.get({
+								resourceName: 'people/' + payload.sub + "?personFields=phoneNumbers"
+							});
+							var tel = "";
+							if (res.data.phoneNumbers != undefined) {
+								tel = res.data.phoneNumbers[0].canonicalForm;
 							}
-						});
-						const res = await service.people.get({
-							resourceName: 'people/' + payload.sub + "?personFields=phoneNumbers"
-						});
-						var tel = "";
-						if(res.data.phoneNumbers != undefined) {
-							tel = res.data.phoneNumbers[0].canonicalForm;
+							user = new Utente({
+								nome: payload.given_name,
+								email: payload.email,
+								password: "",
+								salt: "",
+								tel: tel,
+								profilePic: payload.picture,
+								numEvOrg: 0,
+								valutazioneMedia: 0.0,
+								g_refresh_token: ""
+							});
+							await user.save();
 						}
-						user = new Utente({
-							nome: payload.given_name,
-							email: payload.email,
-							password: "",
-							salt: "",
-							tel: tel,
-							profilePic: payload.picture,
-							numEvOrg: 0,
-							valutazioneMedia: 0.0,
-							g_refresh_token: ""
-						});
-						await user.save();
-					}
-					user = await Utente.findOne({ email: {$eq: payload.email}});
-					res.status(200).json(result(gJwt, payload.email, user.id, payload.picture)).send();
-				})
-				.catch(async err => {
-					console.log(err);
-					var error = false;
-					const resp = await fetch("graph.facebook.com/debug_token?input_token=" + req.body.googleJwt + "&access_token=" + process.env.FACEBOOK_MOBILE_TOKEN)
-					.catch(err => {
-						res.status(500).json({
-							error: "Errore interno al server"
+						user = await Utente.findOne({ email: { $eq: payload.email } });
+						res.status(200).json(result(gJwt, payload.email, user.id, payload.picture)).send();
+					})
+					.catch(async err => {
+						console.log(err);
+						res.status(401).json({
+							error: "Token non valido."
 						}).send();
-						error = true;
 					});
-
-					if(!error) {
-						const json = await resp.json();
-						if(json.data.is_valid) {
-							console.log("scopes: " + json.data);
-							const scopes = json.data.scopes;
-							if(!scopes.includes("email")) {
-								res.status(400).json({
-									error: "L'utente non ha concesso l'autorizzazione per l'email"
-								});
-							} else {
-								await fetch("graph.facebook.com/v15.0/" + json.data.user_id + "?fields=email,name,picture&access_token=" + req.body.googleJwt)
-								.then(async resp => {
-									const json = await resp.json();
-									var user = new Utente({
-										nome: json.data.nome,
-										email: json.data.email,
-										password: "",
-										salt: "",
-										tel: "",
-										profilePic: json.data.picture.data.url,
-										numEvOrg: 0,
-										valutazioneMedia: 0.0,
-										g_refresh_token: ""
-									});
-									await user.save();
-
-									user = await Utente.findOne({ email: {$eq: json.data.email}});
-									res.status(200).json(result(req.body.googleJwt, json.data.email, user.id, json.data.picture.data.url)).send();
-								})
-								.catch(err => {
-									res.status(400).json({
-										error: "OAuth exception"
-									}).send();
-								});
-								
-							}
-						} else {
-							res.status(401).json({
-								error: "Token non valido."
-							}).send();
-						}
-					}
-				});
 				return;
 			}
 
-			//Set Facebook Login
-
-			//No authentication with identity providers, so use email and password
+			//No authentication with Google identity provider, so use email and password
 			const v1 = new Validator({
 				email: req.body.email,
 				password: req.body.password
@@ -206,6 +156,75 @@ router.post('', (req, res) => {
 		})
 		.catch(err => console.log(err));
 	return;
+});
+
+router.post("/facebookLogin", async (req, res) => {
+	const v = new Validator({
+		csrfToken: req.body.csrfToken
+	}, {
+		csrfToken: 'required|string'
+	});
+	v.check()
+		.then(async matched => {
+			if (!matched) {
+				console.log(v.errors);
+				res.status(400).json(result(undefined, undefined, undefined, true, "Errore di autenticazione.")).send();
+				return;
+			}
+			var error = false;
+			const resp = await fetch("graph.facebook.com/debug_token?input_token=" + req.body.googleJwt + "&access_token=" + process.env.FACEBOOK_MOBILE_TOKEN)
+				.catch(err => {
+					res.status(500).json({
+						error: "Errore interno al server"
+					}).send();
+					error = true;
+				});
+
+			if (!error) {
+				await resp.json()
+				.then(async json => {
+					if (json.data.is_valid) {
+						console.log("scopes: " + json.data);
+						const scopes = json.data.scopes;
+						if (!scopes.includes("email")) {
+							res.status(400).json({
+								error: "L'utente non ha concesso l'autorizzazione per l'email"
+							});
+						} else {
+							await fetch("graph.facebook.com/v15.0/" + json.data.user_id + "?fields=email,name,picture&access_token=" + req.body.googleJwt)
+								.then(async resp => {
+									const json = await resp.json();
+									var user = new Utente({
+										nome: json.data.nome,
+										email: json.data.email,
+										password: "",
+										salt: "",
+										tel: "",
+										profilePic: json.data.picture.data.url,
+										numEvOrg: 0,
+										valutazioneMedia: 0.0,
+										g_refresh_token: ""
+									});
+									await user.save();
+	
+									user = await Utente.findOne({ email: { $eq: json.data.email } });
+									res.status(200).json(result(req.body.googleJwt, json.data.email, user.id, json.data.picture.data.url)).send();
+								})
+								.catch(err => {
+									res.status(400).json({
+										error: "OAuth exception"
+									}).send();
+								});
+	
+						}
+					} else {
+						res.status(401).json({
+							error: "Token non valido."
+						}).send();
+					}
+				});
+			}
+		});
 });
 
 export default router;
