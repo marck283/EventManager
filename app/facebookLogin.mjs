ea7eacf77f9a9db7cc1dc0a77ce8056414f3e71f
marck283 <86911602+marck283@users.noreply.github.com>
2022-11-22 18:01:49 +0100

    Added check to check if the user is already registered on Facebook login

--- app/facebookLogin.mjs
+++ app/facebookLogin.mjs
@@ -4,7 +4,11 @@ var login = async (fbUserId, fbJwt, res) => {
     await fetch("https://graph.facebook.com/v15.0/" + fbUserId + "?fields=email,name,picture&access_token=" + fbJwt)
         .then(resp => resp.json()
             .then(async json1 => {
-                console.log(json1);
+                var user1 = await Utente.findOne({ email: { $eq: json1.email } });
+                if(user1 != undefined && user1 != null) {
+                    res.status(409).json({ error: "Utente già registrato." }).send();
+                    return;
+                }
                 var user = new Utente({
                     nome: json1.nome,
                     email: json1.email,
