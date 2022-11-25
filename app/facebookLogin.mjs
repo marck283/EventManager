8b3c40912f63a6c1ab9edaaaae1d8d504a2e4a26
marck283 <86911602+marck283@users.noreply.github.com>
2022-11-25 22:33:48 +0100

    Added checks

--- app/facebookLogin.mjs
+++ app/facebookLogin.mjs
@@ -6,33 +6,38 @@ var login = async (fbUserId, fbJwt, res) => {
         .then(resp => resp.json()
             .then(async json1 => {
                 var user1 = await Utente.findOne({ email: { $eq: json1.email } });
-                if(user1 != undefined && user1 != null) {
-                    res.status(409).json({ error: "Utente già registrato." }).send();
-                    return;
-                }
-                var user = new Utente({
-                    nome: json1.nome,
-                    email: json1.email,
-                    password: "",
-                    salt: "",
-                    tel: "",
-                    profilePic: json1.picture.data.url,
-                    numEvOrg: 0,
-                    valutazioneMedia: 0.0,
-                    googleAccount: null,
-                    facebookAccount: {
-                        userId: fbUserId
+                if (user1 != undefined && user1 != null) {
+                    if(user1.facebookAccount != null && user1.facebookAccount.userId != null &&
+                        user1.facebookAccount.userId != undefined && typeof(user1.facebookAccount.userId) == String &&
+                        user1.facebookAccount.userId != "") {
+                        res.status(409).json({ error: "Utente già registrato." }).send();
+                        return;
                     }
-                });
-                await user.save();
+                } else {
+                    var user = new Utente({
+                        nome: json1.nome,
+                        email: json1.email,
+                        password: "",
+                        salt: "",
+                        tel: "",
+                        profilePic: json1.picture.data.url,
+                        numEvOrg: 0,
+                        valutazioneMedia: 0.0,
+                        googleAccount: null,
+                        facebookAccount: {
+                            userId: fbUserId
+                        }
+                    });
+                    await user.save();
+                }
                 user = await Utente.findOne({ email: { $eq: json1.email } });
                 res.status(200).json(result(createToken(json1.email, user.id, 3600), json1.email, user.id, json1.picture.data.url)).send();
             }))
-            .catch(err => {
-                res.status(404).json({
-                    error: "Utente non trovato."
-                }).send();
-            });
+        .catch(err => {
+            res.status(404).json({
+                error: "Utente non trovato."
+            }).send();
+        });
 };
 
 export default login;
