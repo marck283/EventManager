5efbc34f09310e78ea7f03b4930f093bc165d8f4
marck283 <86911602+marck283@users.noreply.github.com>
2023-01-29 02:01:56 +0100

    Riformulata procedura ottenimento eventi organizzati per la data selezionata

--- app/events/elencoEventiOrg.mjs
+++ app/events/elencoEventiOrg.mjs
@@ -9,8 +9,8 @@ import map from './eventsMap.mjs';
 import User from '../collezioni/utenti.mjs';
 import getOrgNames from './OrgNames.mjs';
 
-var limiter = RateLimit ({
-    windowMs: 1*60*1000, //1 minute
+var limiter = RateLimit({
+    windowMs: 1 * 60 * 1000, //1 minute
     max: 100, //Limit each IP to 10 requests per minute
     message: async () => "Hai raggiunto il numero massimo di richieste al minuto.",
     statusCode: 429
@@ -26,7 +26,7 @@ var findEvents = async (arr, obj, data) => {
 };
 
 var mapAndPush = async (arr, genArr, cat) => {
-    if(arr != null && arr != undefined && arr.length > 0) {
+    if (arr != null && arr != undefined && arr.length > 0) {
         let events = map(arr, cat, await getOrgNames(arr));
         events.forEach(e => genArr.push(e));
     }
@@ -38,42 +38,30 @@ var mapAndPush = async (arr, genArr, cat) => {
 
 router.get("/:data", async (req, res) => {
     var data = req.params.data;
-    const v = new Validator({
-        utent: req.loggedUser.id || req.loggedUser.sub
-    }, {
-        utent: 'required|string|minLength:1'
-    });
-    v.check()
-    .then(async matched => {
-        if(!matched) {
-            res.status(400).json({error: "Richiesta malformata."});
-            return;
-        }
-        var utent = req.loggedUser.id || req.loggedUser.sub, eventList, eventsPers, eventsPriv;
-        let obj;
+    var utent = req.loggedUser.id || req.loggedUser.sub, eventList, eventsPers, eventsPriv;
+    let obj;
 
-        console.log("User1:", utent);
-        if(utent === req.loggedUser.id) {
-            obj = {organizzatoreID: {$eq: utent}};
-        } else {
-            utent = await User.findOne({email: {$eq: req.loggedUser.email}});
-            console.log("User:", utent);
-            obj = {organizzatoreID: {$eq: utent.id}};
-        }
-        eventList = await findEvents(eventPublic, obj, data);
-        eventsPers = await findEvents(eventPers, obj, data);
-        eventsPriv = await findEvents(eventPriv, obj, data);
+    console.log("User1:", utent);
+    if (utent === req.loggedUser.id) {
+        obj = { organizzatoreID: { $eq: utent } };
+    } else {
+        utent = await User.findOne({ email: { $eq: req.loggedUser.email } });
+        console.log("User:", utent);
+        obj = { organizzatoreID: { $eq: utent.id } };
+    }
+    eventList = await findEvents(eventPublic, obj, data);
+    eventsPers = await findEvents(eventPers, obj, data);
+    eventsPriv = await findEvents(eventPriv, obj, data);
 
-        eventList = await mapAndPush(eventList, [], "pub");
-        eventList = await mapAndPush(eventsPers, eventList, "pers");
-        eventList = await mapAndPush(eventsPriv, eventList, "priv");
+    eventList = await mapAndPush(eventList, [], "pub");
+    eventList = await mapAndPush(eventsPers, eventList, "pers");
+    eventList = await mapAndPush(eventsPriv, eventList, "priv");
 
-        if(eventList != null && eventList != undefined && eventList.length > 0) {
-            res.status(200).json({eventi: eventList}).send();
-        } else {
-            res.status(404).json({error: "Nessun evento organizzato da questo utente."}).send();
-        }
-    });
+    if (eventList != null && eventList != undefined && eventList.length > 0) {
+        res.status(200).json({ eventi: eventList }).send();
+    } else {
+        res.status(404).json({ error: "Nessun evento organizzato da questo utente." }).send();
+    }
 });
 
 export default router;
