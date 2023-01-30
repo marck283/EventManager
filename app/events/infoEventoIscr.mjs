198b84c1daa000bea81219fc5f7f3b8d21d0fc98
marck283 <86911602+marck283@users.noreply.github.com>
2023-01-30 01:29:53 +0100

    Added code to reduce amount of returned data when searching for information on an event to which the user is subscribed

--- app/events/infoEventoIscr.mjs
+++ app/events/infoEventoIscr.mjs
@@ -7,6 +7,8 @@ import eventPriv from '../collezioni/eventPrivat.mjs';
 import map from './eventsMap.mjs';
 import User from '../collezioni/utenti.mjs';
 import getOrgNames from './OrgNames.mjs';
+import { Validator } from 'node-input-validator';
+import test from '../hourRegexTest.mjs';
 
 var limiter = RateLimit ({
     windowMs: 1*60*1000, //1 minute
@@ -19,7 +21,7 @@ var limiter = RateLimit ({
 //Avoids Denial of Service attacks by limiting the number of requests per IP
 router.use(limiter);
 
-var findEvents = async id => {
+var findEvent = async id => {
     let eventoPub = await eventPublic.findById(id), eventoPriv = await eventPriv.findById(id);
     let eventoPers = await eventPers.findById(id);
 
@@ -42,12 +44,38 @@ router.get("/:id", async (req, res) => {
         user = (await User.findOne({email: {$eq: req.loggedUser.email}})).id;
     }
 
-    let event = await findEvents(req.params.id);
-    if(event != null && event != undefined) {
-        res.status(200).json({event: event}).send();
-    } else {
-        res.status(404).json({error: "Evento non trovato"}).send();
-    }
+    const v = new Validator({
+        data: req.headers.data,
+        ora: req.headers.ora
+    }, {
+        data: 'required|string|dateFormat:MM-DD-YYYY',
+        ora: 'required|string|minLength:5|maxLength:5'
+    });
+    v.check()
+    .then(async matched => {
+        if(!matched) {
+            res.status(400).json({error: "Errore di validazione"}).send();
+            return;
+        }
+        if(!test(req.headers.ora)) {
+            res.status(400).json({error: "Ora non valida"}).send();
+            return;
+        }
+        let event = await findEvent(req.params.id);
+        if(event != null && event != undefined) {
+            let result = {};
+            for(let key in event) {
+                if(key === "luogoEv") {
+                    result[key] = event[key].luogoEv.filter(luogo => luogo.data === req.headers.data && luogo.ora === req.headers.ora);
+                } else {
+                    result[key] = event[key];
+                }
+            }
+            res.status(200).json({event: result}).send();
+        } else {
+            res.status(404).json({error: "Evento non trovato"}).send();
+        }
+    });
 });
 
 export default router;
