375647ab9996a001c55fb141486d1e5df3591c41
marck283 <86911602+marck283@users.noreply.github.com>
2023-01-31 19:28:28 +0100

    Added checks on QR codes

--- app/events/checkQR.mjs
+++ app/events/checkQR.mjs
@@ -3,7 +3,8 @@ import RateLimit from 'express-rate-limit';
 const router = Router();
 import { Validator } from 'node-input-validator';
 import biglietti from '../collezioni/biglietti.mjs';
-import fromDataUrl from 'qrcode';
+import User from '../collezioni/utenti.mjs';
+import hourCheck from '../hourRegexTest.mjs';
 
 var limiter = RateLimit({
     windowMs: 1 * 60 * 1000, //1 minute
@@ -17,20 +18,35 @@ var limiter = RateLimit({
 router.use(limiter);
 
 router.get("/:qrcode", async (req, res) => {
+    var user = req.loggedUser.id || req.loggedUser.sub;
+
+    if(user === req.loggedUser.sub) {
+        user = (await User.findOne({email: {$eq: req.loggedUser.email}})).id;
+    }
+
     const v = new Validator({
-        qr_code: req.params.qrcode
+        qr_code: req.params.qrcode,
+        eventoid: req.headers.eventoid,
+        day: req.headers.day,
+        hour: req.headers.hour
     }, {
-        qr_code: 'required|string|minLength:1'
+        qr_code: 'required|string|minLength:1',
+        eventoid: 'required|string|minLength:1',
+        day: 'required|string|dateFormat:MM-DD-YYYY',
+        hour: 'required|string|minLength:5|maxLength:5'
     });
 
     v.check()
     .then(async matched => {
-        if(!matched) {
+        if(!matched || !hourCheck(req.headers.hour)) {
             res.status(400).json({error: "QR Code non valido. Riprova."}).send();
             return;
         }
         console.log(req.params.qrcode);
-        const biglietto = await biglietti.findOne({qr: {$eq: req.params.qrcode}});
+
+        const biglietto = await biglietti.findOne({qr: {$eq: req.params.qrcode},
+            eventoid: {$eq: req.headers.eventoid}, utenteid: {$eq: user}, giorno: {$eq: req.headers.day},
+            ora: {$eq: req.headers.hour}});
         if(biglietto != null && biglietto != undefined) {
             res.status(200).json({status: "OK"}).send();
         } else {
