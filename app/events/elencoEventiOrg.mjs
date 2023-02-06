cf32fa71b49dc4dfb6f5436d0206d896c8ac324c
marck283 <86911602+marck283@users.noreply.github.com>
2023-02-06 17:20:31 +0100

    Added print statement

--- app/events/elencoEventiOrg.mjs
+++ app/events/elencoEventiOrg.mjs
@@ -76,6 +76,8 @@ router.get("/:data", async (req, res) => {
 router.get("", async (req, res) => {
     var utent = req.loggedUser.id || req.loggedUser.sub;
 
+    console.log(utent);
+
     if (utent == req.loggedUser.sub) {
         utent = (await User.findOne({ email: { $eq: req.loggedUser.email } })).id;
     }
