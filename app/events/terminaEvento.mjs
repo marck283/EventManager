f10c899ec84539458a02ac074d80e32057d9d231
marck283 <86911602+marck283@users.noreply.github.com>
2023-02-08 03:09:50 +0100

    Modified event search and update on events that are about to be terminated

--- app/events/terminaEvento.mjs
+++ app/events/terminaEvento.mjs
@@ -2,6 +2,7 @@ import { Router, json } from 'express';
 import eventPublic from '../collezioni/eventPublic.mjs';
 const router = Router();
 import { Validator } from 'node-input-validator';
+import ObjectId from 'mongoose';
 
 router.use(json({ limit: "50mb" })); //Limiting the size of the request should avoid "Payload too large" errors
 
@@ -21,14 +22,16 @@ router.patch("/:id", async (req, res) => {
                 return;
             }
     
-            let eventoPub = await eventPublic.findById(req.params.id);
-            if(eventoPub == undefined) {
+            /*let eventoPub = */await eventPublic.findOneAndUpdate({_id: {$eq: new ObjectId(req.params.id)},
+                "luogoEv.data": {$eq: req.body.data}, "luogoEv.ora": {$eq: req.body.ora}},
+                {$set: {"luogoEv.$.terminato": true}});
+            /*if(eventoPub == undefined) {
                 res.status(404).json({error: "Evento non trovato."}).send();
                 return;
             }
             eventoPub.luogoEv.filter(e => e.data == req.headers.data && e.ora == req.headers.ora)[0].terminato = true;
             console.log(eventoPub);
-            await eventoPub.save();
+            await eventoPub.save();*/
         });
         res.status(200).json({message: "Evento terminato con successo."}).send();
     } catch(err) {
