0212c6e6b809a741df10f7362bacfdb6ad43ea76
marck283 <86911602+marck283@users.noreply.github.com>
2023-02-08 03:21:06 +0100

    Removed comment

--- app/events/terminaEvento.mjs
+++ app/events/terminaEvento.mjs
@@ -22,16 +22,9 @@ router.patch("/:id", async (req, res) => {
                 return;
             }
     
-            /*let eventoPub = */await eventPublic.findOneAndUpdate({_id: {$eq: new mongoose.Types.ObjectId(req.params.id)},
+            await eventPublic.findOneAndUpdate({_id: {$eq: new mongoose.Types.ObjectId(req.params.id)},
                 "luogoEv.data": {$eq: req.body.data}, "luogoEv.ora": {$eq: req.body.ora}},
                 {$set: {"luogoEv.$.terminato": true}});
-            /*if(eventoPub == undefined) {
-                res.status(404).json({error: "Evento non trovato."}).send();
-                return;
-            }
-            eventoPub.luogoEv.filter(e => e.data == req.headers.data && e.ora == req.headers.ora)[0].terminato = true;
-            console.log(eventoPub);
-            await eventoPub.save();*/
         });
         res.status(200).json({message: "Evento terminato con successo."}).send();
     } catch(err) {
