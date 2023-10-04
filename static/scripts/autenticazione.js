07b00e2015f462e8df223ac21bb8b176859ed0af
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2023-10-04 23:52:33 +0200

    Added authenticaton via form #2

--- static/scripts/autenticazione.js
+++ static/scripts/autenticazione.js
@@ -52,7 +52,7 @@ var login = (gresponse = null) => {
         //tokenChecker module.
         localStorage.setItem('token', gresponse);
     }
-    fetch('../api/v2/authentications', {
+    fetch('/api/v2/authentications', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
