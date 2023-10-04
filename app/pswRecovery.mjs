7054fffc639a46017fa611fee0a900e58905bd99
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2023-10-04 05:43:34 +0200

    Added password reset functionality

--- app/pswRecovery.mjs
+++ app/pswRecovery.mjs
@@ -1,8 +1,60 @@
 import { Router } from 'express';
 const router = Router();
+import { Validator } from 'node-input-validator';
+import { SMTPClient } from 'emailjs';
+import Utente from './collezioni/utenti.mjs';
+
+const client = new SMTPClient({
+	user: process.env.ADMIN_USER,
+	password: process.env.EMAIL_PASS,
+	host: 'smtp.gmail.com',
+	ssl: true,
+});
 
 router.post('', async (req, res) => {
     //Implement mail sending
+    const v = new Validator({
+        user: req.body.email
+    }, {
+        user: 'required|email'
+    });
+    v.check().then(async matched => {
+        if(!matched) {
+            //Send error message
+            console.log(v.errors);
+            res.status(400).json({ error: "Indirizzo email non fornito o non valido." }).send();
+            return;
+        }
+        let userTo = req.body.email;
+        let user = await Utente.findOne({email: {$eq: userTo}});
+        if(!user) {
+            res.status(404).json({error: "Utente non trovato."}).send();
+            return;
+        }
+        let emailText = "Dear " + userTo + ",\nIn order to reset Your password, go to https://eventmanager-uo29.onrender.com/pswRecovery.html.\
+        \nBest regards!\n\nThe EventManager development team";
+        try {
+            await client.sendAsync({
+                text: emailText,
+                from: process.env.ADMIN_EMAIL,
+                to: userTo,
+                subject: 'Password reset',
+            });
+            console.log("EMAIL SENT");
+            res.status(201)
+            .json({message: "Un'email è stata appena inviata alla tua casella di posta elettronica. Se non la trovi, prova a cercare nelle cartelle Spam e Cestino."})
+            .send();
+        } catch (err) {
+            res.status(500).json("Internal server error.").send();
+            console.log(err);
+            return;
+        }
+    })
+    .catch(error => {
+        res.status(500).json("Internal server error.").send();
+        console.log(error);
+        return;
+    })
 });
 
 export default router;
---
+++ app/tests/sendNewPsw.test.mjs
@@ -0,0 +1,44 @@
+import request from 'supertest';
+import app from '../app.mjs';
+import {jest} from '@jest/globals';
+import Utente from '../collezioni/utenti.mjs';
+
+describe('POST /api/v2/RecuperoPassword', () => {
+    beforeAll(() => {
+        const userMock = jest.spyOn(Utente, "findOne").mockImplementation(criterias => {
+            if(criterias.email.$eq == "aabb12@gmail.com") {
+                return {
+                    _id: 6542,
+                    user: "aabb12@gmail.com"
+                };
+            }
+            return null;
+        })
+    });
+
+    afterAll(() => {
+        jest.restoreAllMocks();
+    });
+
+    it("POST /api/v2/RecuperoPassword test reset password", async () => {
+        await request(app).post("/api/v2/RecuperoPassword")
+        .set('Accept', 'application/json')
+        .send({email: "aabb12@gmail.com"})
+        .expect('Content-Type', /json/)
+        .expect(201, {message: "Un'email è stata appena inviata alla tua casella di posta elettronica. Se non la trovi, prova a cercare nelle cartelle Spam e Cestino."});
+    });
+
+    it("POST /api/v2/RecuperoPassword test reset password con utente non trovato", async () => {
+        await request(app).post("/api/v2/RecuperoPassword")
+        .set('Accept', 'application/json')
+        .send({email: "marvel00.ml1@gmail.com"})
+        .expect('Content-Type', /json/)
+        .expect(404, {error: "Utente non trovato."});
+    });
+
+    it("POST /api/v2/RecuperoPassword test reset password senza email", async () => {
+        await request(app).post("/api/v2/RecuperoPassword")
+        .expect('Content-Type', /json/)
+        .expect(400, {error: "Indirizzo email non fornito o non valido."});
+    });
+})
